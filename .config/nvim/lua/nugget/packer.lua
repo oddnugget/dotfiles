9d73b8ba798753594afd13e9471e067b5d9586b2
odyrag <odysseas.ragnarson@auctionet.com>
2022-08-02 09:29:28 +0200

    fix bug

--- .config/nvim/lua/nugget/packer.lua
+++ .config/nvim/lua/nugget/packer.lua
@@ -8,14 +8,14 @@ return require('packer').startup(function(use)
         'nvim-telescope/telescope.nvim', 
         requires = {{ 'nvim-lua/plenary.nvim' }},
         config = function()
-            require("config.telescope")
+            require("nugget.config.telescope")
         end
     }
 
     use {
         'folke/tokyonight.nvim', 
         config = function() 
-            require("config.colorscheme") 
+            require("nugget.config.colorscheme") 
         end 
     }
 end)
