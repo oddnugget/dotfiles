9f66aa36036a0d8dcee816243613df9071d8006a
odyrag <odysseas.ragnarson@auctionet.com>
2022-08-04 09:52:46 +0200

    fix

---
+++ .config/nvim/lua/nugget/config/lsp/init.lua
@@ -0,0 +1,90 @@
+local lspconfig = require("lspconfig")
+local required_servers = require("nugget.lsp_servers")
+local M = {}
+
+require("nvim-lsp-installer").setup({
+  ensure_installed = required_servers,
+})
+
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set("n", "<leader>de", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+capabilities.textDocument.completion.completionItem = {
+  documentationFormat = { "markdown", "plaintext" },
+  snippetSupport = true,
+  preselectSupport = true,
+  insertReplaceSupport = true,
+  labelDetailsSupport = true,
+  deprecatedSupport = true,
+  commitCharactersSupport = true,
+  tagSupport = { valueSet = { 1 } },
+  resolveSupport = {
+    properties = {
+      "documentation",
+      "detail",
+      "additionalTextEdits",
+    },
+  },
+}
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  if client.name ~= "null-ls" then
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+  end
+  local bufopts = { noremap = true, buffer = bufnr }
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "gh", vim.lsp.buf.hover, bufopts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+  vim.keymap.set("n", "<leader>f", vim.lsp.buf.formatting, bufopts)
+end
+
+local settings = {
+  sumneko_lua = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+        },
+        maxPreload = 100000,
+        preloadFileSize = 10000,
+      },
+    },
+  },
+}
+
+function M.setup()
+  require("nvim-lsp-installer").setup({
+    ensure_installed = required_servers,
+  })
+
+  for _, server in ipairs(required_servers) do
+    local server_opts = {
+      on_attach = on_attach,
+      settings = settings[server] or nil,
+      capabilities = capabilities,
+    }
+
+    lspconfig[server].setup(server_opts)
+  end
+end
+
+return M
