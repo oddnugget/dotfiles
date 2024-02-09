eb14ef00c2b15303cf30fa62e65c79683050ae93
Odysseas Ragnarson <odysseas.ragnarson@auctionet.com>
2024-02-09 15:47:25 +0100

    remove cmp commandline autocomplete

--- nvim/lua/oddnugget/plugins/completion.lua
+++ nvim/lua/oddnugget/plugins/completion.lua
@@ -98,33 +98,6 @@ return {
 					end,
 				},
 			})
-
-			-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-			cmp.setup.cmdline({ "/", "?" }, {
-				mapping = cmp.mapping.preset.cmdline(),
-				completion = {
-					completeopt = "menuone,noselect",
-				},
-				sources = {
-					{ name = "buffer" },
-				},
-			})
-
-			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				completion = {
-					completeopt = "menuone,noselect",
-				},
-				sources = {
-					{
-						name = "cmdline",
-						option = {
-							ignore_cmds = { "Man", "!" },
-						},
-					},
-				},
-			})
 		end,
 	},
 	{
