0b2ac5e010beea091cc70f853d18599ee00b546d
Odysseas Ragnarson <odysseas.ragnarson@auctionet.com>
2024-02-12 11:32:49 +0100

    wilder fuzzy

--- nvim/lua/oddnugget/plugins/wilder.lua
+++ nvim/lua/oddnugget/plugins/wilder.lua
@@ -15,6 +15,19 @@ return {
 			})
 
 			local wilder = require("wilder")
+
+			wilder.set_option("pipeline", {
+				wilder.branch(
+					wilder.cmdline_pipeline({
+						fuzzy = 1,
+						set_pcre2_pattern = 1,
+					}),
+					wilder.python_search_pipeline({
+						pattern = "fuzzy",
+					})
+				),
+			})
+
 			local highlighters = {
 				wilder.basic_highlighter(),
 			}
