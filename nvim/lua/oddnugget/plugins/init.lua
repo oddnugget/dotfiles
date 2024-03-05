090816f990acb87dedb696f08fcc28cbbdcdf9b3
Odysseas Ragnarson <odysseas.ragnarson@auctionet.com>
2024-03-05 08:44:03 +0100

    smart-splits

--- nvim/lua/oddnugget/plugins/init.lua
+++ nvim/lua/oddnugget/plugins/init.lua
@@ -2,27 +2,12 @@ return {
 	"nvim-lua/plenary.nvim",
 	"MunifTanjim/nui.nvim",
 	-- { "christoomey/vim-tmux-navigator", lazy = false },
-	-- {
-	-- 	lazy = false,
-	-- 	"numToStr/Navigator.nvim",
-	-- 	config = function()
-	-- 		require("Navigator").setup()
-	-- 		local opts = { noremap = true }
-
-	-- 		vim.keymap.set({ "n", "t" }, "<C-h>", "<CMD>NavigatorLeft<CR>", opts)
-	-- 		vim.keymap.set({ "n", "t" }, "<C-l>", "<CMD>NavigatorRight<CR>", opts)
-	-- 		vim.keymap.set({ "n", "t" }, "<C-j>", "<CMD>NavigatorDown<CR>", opts)
-	-- 		vim.keymap.set({ "n", "t" }, "<C-k>", "<CMD>NavigatorUp<CR>", opts)
-	-- 	end,
-	-- },
 	{
-		"mrjones2014/smart-splits.nvim",
+		-- "mrjones2014/smart-splits.nvim",
+		dir = "/projects/nvim-forks/smart-splits.nvim",
 		lazy = false,
 		config = function()
-			require("smart-splits").setup()
-			-- recommended mappings
-			-- resizing splits
-			-- these keymaps will also accept a range,
+			require("smart-splits").setup({ log_level = "trace", at_edge = "stop" })
 			-- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
 			-- vim.keymap.set("n", "<SC-h>", require("smart-splits").resize_left)
 			-- vim.keymap.set("n", "<SC-j>", require("smart-splits").resize_down)
@@ -33,12 +18,6 @@ return {
 			vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
 			vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
 			vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
-			vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
-			-- swapping buffers between windows
-			vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
-			vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
-			vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
-			vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
 		end,
 	},
 	{ "tpope/vim-fugitive", lazy = false },
