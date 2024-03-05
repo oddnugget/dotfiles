e8428a13c49648b731f9168d9d5cda6ae6ff0dca
odyrag <odysseas.ragnarson@auctionet.com>
2024-03-05 08:41:05 +0100

    wez mux fixes

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,4 @@
 local wezterm = require("wezterm")
-local act = wezterm.action
 
 ------------------------ smart-splits.nvim --------------------------------------------
 -- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
@@ -15,13 +14,18 @@ local direction_keys = {
 	l = "Right",
 }
 
+local log = wezterm.log_info
+
 local function split_nav(resize_or_move, key)
 	return {
 		key = key,
 		mods = resize_or_move == "resize" and "META" or "CTRL",
 		action = wezterm.action_callback(function(win, pane)
+			log(pane:get_user_vars())
+
 			if is_vim(pane) then
 				-- pass the keys through to vim/nvim
+
 				win:perform_action({
 					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
 				}, pane)
@@ -36,49 +40,13 @@ local function split_nav(resize_or_move, key)
 	}
 end
 
------------------------- Conditional Pane Activation (custom)  ------------------------
---
--- local function isViProcess(pane)
--- 	local nvim = pane:get_user_vars()["WEZTERM_PROG"]
--- 	return nvim:find("n?vim") ~= nil
--- end
---
--- local function conditionalActivatePane(window, pane, pane_direction, vim_direction)
--- 	wezterm.log_info(pane:get_user_vars())
--- 	if isViProcess(pane) then
--- 		wezterm.log_info("is vi process")
--- 		window:perform_action(
--- 			-- This should match the keybinds you set in Neovim.
--- 			act.SendKey({ key = vim_direction, mods = "CTRL" }),
--- 			pane
--- 		)
--- 	else
--- 		window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
--- 	end
--- end
-
--- wezterm.on("ActivatePaneDirection-right", function(window, pane)
--- 	conditionalActivatePane(window, pane, "Right", "l")
--- end)
-
--- wezterm.on("ActivatePaneDirection-left", function(window, pane)
--- 	conditionalActivatePane(window, pane, "Left", "h")
--- end)
-
--- wezterm.on("ActivatePaneDirection-up", function(window, pane)
--- 	conditionalActivatePane(window, pane, "Up", "k")
--- end)
-
--- wezterm.on("ActivatePaneDirection-down", function(window, pane)
--- 	conditionalActivatePane(window, pane, "Down", "j")
--- end)
-
 local config = {
 	font = wezterm.font_with_fallback({
 		"Iosevka Term SS14 Light",
 		"JetBrainsMono Nerd Font",
 		window_background_opacity = 0.95,
 	}),
+	-- debug_key_events = true,
 	font_size = 16.0,
 	hide_tab_bar_if_only_one_tab = false,
 	tab_bar_at_bottom = true,
@@ -87,8 +55,8 @@ local config = {
 	use_fancy_tab_bar = true,
 	window_decorations = "RESIZE",
 	audible_bell = "Disabled",
-	send_composed_key_when_left_alt_is_pressed = false,
-	force_reverse_video_cursor = true,
+	-- send_composed_key_when_left_alt_is_pressed = false,
+	-- force_reverse_video_cursor = true,
 	color_scheme = "Kanagawa (Gogh)",
 	unix_domains = {
 		{ name = "unix" },
@@ -110,23 +78,18 @@ config.window_frame = {
 
 config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-	split_nav("move", "h"),
-	split_nav("move", "j"),
-	split_nav("move", "k"),
-	split_nav("move", "l"),
-	-- resize panes
-	split_nav("resize", "h"),
-	split_nav("resize", "j"),
-	split_nav("resize", "k"),
-	split_nav("resize", "l"),
+	-- split_nav("move", "h"),
+	-- split_nav("move", "j"),
+	-- split_nav("move", "k"),
+	-- split_nav("move", "l"),
 
 	{ key = '"', mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
 	{ key = "%", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
 	-- { key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-	-- { key = "h", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-left") },
-	-- { key = "j", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-down") },
-	-- { key = "k", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-up") },
-	-- { key = "l", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-right") },
+	{ key = "h", mods = "SHIFT|CMD", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+	{ key = "j", mods = "SHIFT|CMD", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+	{ key = "k", mods = "SHIFT|CMD", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+	{ key = "l", mods = "SHIFT|CMD", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
 	{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
 	{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
 	{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
