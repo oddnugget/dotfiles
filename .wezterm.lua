5348c7751c8f9dbc4ccefe200549c492ea417538
odyrag <odysseas.ragnarson@auctionet.com>
2024-03-09 13:03:22 +0100

    turn off native tabs

--- .wezterm.lua
+++ .wezterm.lua
@@ -51,7 +51,7 @@ local config = {
 	hide_tab_bar_if_only_one_tab = false,
 	tab_bar_at_bottom = true,
 	use_dead_keys = false,
-	enable_tab_bar = true,
+	enable_tab_bar = false,
 	use_fancy_tab_bar = true,
 	window_decorations = "RESIZE",
 	audible_bell = "Disabled",
@@ -68,7 +68,7 @@ config.window_padding = {
 	left = 20,
 	right = 10,
 	bottom = 0,
-	top = 20,
+	top = 40,
 }
 
 config.window_frame = {
@@ -76,32 +76,32 @@ config.window_frame = {
 	font = wezterm.font("JetBrainsMono Nerd Font"),
 }
 
-config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
+-- config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
 	-- split_nav("move", "h"),
 	-- split_nav("move", "j"),
 	-- split_nav("move", "k"),
 	-- split_nav("move", "l"),
 
-	{ key = '"', mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-	{ key = "%", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
-	-- { key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-	{ key = "h", mods = "SHIFT|CMD", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-	{ key = "j", mods = "SHIFT|CMD", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
-	{ key = "k", mods = "SHIFT|CMD", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-	{ key = "l", mods = "SHIFT|CMD", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-	{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
-	{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
-	{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
-	{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
-	{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
-	{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
-	{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
-	{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
-	{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
-	{ key = "n", mods = "LEADER", action = wezterm.action({ ActivateTabRelative = 1 }) },
-	{ key = "p", mods = "LEADER", action = wezterm.action({ ActivateTabRelative = -1 }) },
-	{ key = "d", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+	-- { key = '"', mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+	-- { key = "%", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+	-- -- { key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+	-- { key = "h", mods = "SHIFT|CMD", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+	-- { key = "j", mods = "SHIFT|CMD", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+	-- { key = "k", mods = "SHIFT|CMD", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+	-- { key = "l", mods = "SHIFT|CMD", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+	-- { key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
+	-- { key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
+	-- { key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
+	-- { key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
+	-- { key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
+	-- { key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
+	-- { key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
+	-- { key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
+	-- { key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
+	-- { key = "n", mods = "LEADER", action = wezterm.action({ ActivateTabRelative = 1 }) },
+	-- { key = "p", mods = "LEADER", action = wezterm.action({ ActivateTabRelative = -1 }) },
+	-- { key = "d", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
 }
 
 return config
