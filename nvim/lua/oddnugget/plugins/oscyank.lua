a753abcd617d750d09d36daf05aeda815bed21a6
Odysseas Ragnarson <odysseas.ragnarson@auctionet.com>
2024-04-01 16:27:08 +0200

    configure osc52

--- nvim/lua/oddnugget/plugins/oscyank.lua
+++ nvim/lua/oddnugget/plugins/oscyank.lua
@@ -2,15 +2,24 @@ return {
 	"ojroques/nvim-osc52",
 	lazy = false,
 	enabled = function()
-		return os.getenv("DEVBOOX") == "true"
+		return os.getenv("DEVBOX") == "true"
 	end,
 	config = function()
-		local function copy()
-			if vim.v.event.regname == "+" then
-				require("osc52").copy_register("+")
-			end
+		local osc52 = require("osc52")
+		osc52.setup({ silent = true })
+
+		local function copy(lines, _)
+			osc52.copy(table.concat(lines, "\n"))
 		end
 
-		vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
+		local function paste()
+			return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
+		end
+
+		vim.g.clipboard = {
+			name = "osc52",
+			copy = { ["+"] = copy, ["*"] = copy },
+			paste = { ["+"] = paste, ["*"] = paste },
+		}
 	end,
 }
