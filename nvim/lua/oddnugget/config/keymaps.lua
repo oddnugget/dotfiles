27d789efd5740e686c3e56f1c6540a1523b21d28
odyrag <odysseas.ragnarson@auctionet.com>
2024-06-11 10:32:02 +0300

    change jk to jj

--- nvim/lua/oddnugget/config/keymaps.lua
+++ nvim/lua/oddnugget/config/keymaps.lua
@@ -12,8 +12,9 @@ keymap("n", "g,", "g,zvzz")
 keymap("n", "g;", "g;zvzz")
 
 -- Better escape using jk in insert and terminal mode
-keymap("i", "jk", "<ESC>")
-keymap("t", "jk", "<C-\\><C-n>")
+keymap("i", "jj", "<ESC>")
+keymap("t", "jj", "<C-\\><C-n>")
+
 keymap("t", "<C-h>", "<C-\\><C-n><C-w>h")
 keymap("t", "<C-j>", "<C-\\><C-n><C-w>j")
 keymap("t", "<C-k>", "<C-\\><C-n><C-w>k")
